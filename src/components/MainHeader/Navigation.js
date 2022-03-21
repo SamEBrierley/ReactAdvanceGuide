9474147998ebcba2c4f40b3c84e9e8f93e5c85f1
SamEBrierley <samellisbrierley@gmail.com>
2022-03-21 14:19:21 +0000

    Set up the initial project files for a Log in system

---
+++ src/components/MainHeader/Navigation.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import classes from './Navigation.module.css'
+
+const Navigation = (props) =>{
+    return(
+      <nav className={classes.nav}>
+        <ul>
+          {props.isLoggedIn && (
+            <li>
+              <a href="/">User</a>
+            </li>
+          )}
+          {props.isLoggedIn &&(
+          <li>
+            <a href="/">Admin</a>
+            </li>
+          )}
+          {props.isLoggedIn && (
+            <button onClick={props.onLogout}>Logout</button>
+          )}
+        </ul>
+      </nav>
+    )
+}
+
+export default Navigation;
