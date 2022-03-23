0f99bdc61ffc4facacefe2ecd767579839205fa5
SamEBrierley <samellisbrierley@gmail.com>
2022-03-23 10:31:10 +0000

    Added a way to remember a user was logged in when the page is refresed or started again

--- src/App.js
+++ src/App.js
@@ -1,29 +1,39 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from "react";
 
-import Login from './components/Login/Login'
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
-function App(){
+function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const loginHandler = (email, password) =>{
-    setIsLoggedIn(true);
-  }
+	useEffect(() => {
+		const storedUserLoggedInInfo = localStorage.getItem("isLoggedIn");
 
-  const logoutHandler = () =>{
-    setIsLoggedIn(false);
-  }
+		if (storedUserLoggedInInfo === "1") {
+			setIsLoggedIn(true);
+		}
+	}, []);
 
-  return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
-      <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler}/>}
-      </main>
-    </React.Fragment>
-  )
+	const loginHandler = (email, password) => {
+		localStorage.setItem("isLoggedIn", "1");
+		setIsLoggedIn(true);
+	};
+
+	const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
+		setIsLoggedIn(false);
+	};
+
+	return (
+		<React.Fragment>
+			<MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+			<main>
+				{!isLoggedIn && <Login onLogin={loginHandler} />}
+				{isLoggedIn && <Home onLogout={logoutHandler} />}
+			</main>
+		</React.Fragment>
+	);
 }
 
 export default App;
