c7243d7a456f6b5518e151d34bc47a211fe4a055
SamEBrierley <samellisbrierley@gmail.com>
2022-04-07 11:37:26 +0100

    Moved the location of the log in status to authcontect.js in the store folder

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useContect } from 'react';
 
 import Card from '../UI/Card/Card';
+import Button from '../UI/Button/Button';
 import classes from './Home.module.css';
+import AuthContext from '../../store/auth-context';
 
 const Home = (props) => {
+  const authCtx = useContext(AuthContext);
+
   return(
     <Card className={classes.home}>
       <h1>Welcome Back!</h1>
+      <Button onClick={authCtx.onLogout}>Logout</Button>
     </Card>
-  )
-}
+  );
+};
+
 export default Home;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
 import './index.css';
 import App from './App';
+import { AuthContextProvider } from './store/auth-cintext';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+ShadowRoot.render(
+    <AuthContextProvider>
+        <App />
+    </AuthContextProvider>
+);
